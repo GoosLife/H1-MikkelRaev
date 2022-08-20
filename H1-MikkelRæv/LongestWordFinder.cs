894b3537577a2946fc8aaee238f26f8466d00889
GoosLife <72657308+GoosLife@users.noreply.github.com>
2022-08-20 15:42:55 +0200

    Add project files.

---
+++ H1-MikkelRæv/LongestWordFinder.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_MikkelRæv
+{
+    internal class LongestWordFinder
+    {
+        // Remove all punctuation from the words, to ensure that the results aren't being padded by punctuation.
+        public string StripPunctuation(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            // Check each char in the string
+            foreach (char c in s)
+            {
+                // Append all the chars that aren't punctuation to the return value
+                if (!char.IsPunctuation(c))
+                    sb.Append(c);
+            }
+            
+            return sb.ToString();
+        }
+
+        public List<string> FindLongestWord(string[] s)
+        {
+            // The length of the currently longest word(s).
+            int longestWordLength = 0;
+            // The currently longest word(s).
+            List<string> longestWords = new List<string>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                // Remove punctuation from each word, and avoid duplication by turning all letters lowercase.
+                s[i] = StripPunctuation(s[i].ToLower());
+
+                // If the current word is the longest we've come across yet, save a copy of it, and update the length required to beat it.
+                if (s[i].Length > longestWordLength)
+                {
+                    longestWordLength = s[i].Length;
+                    longestWords.Clear(); // Clear the list, remove all existing entries. Only words of the current highest length can be in the list.
+                    longestWords.Add(s[i]);
+                }
+                // If the word is equally as long as the currently longest word, add it at the end to return all words of the longest length.
+                else if (s[i].Length == longestWordLength)
+                {
+                    // Avoid duplicate entries by only adding words that don't already exist.
+                    if (!longestWords.Contains(s[i]))
+                    {
+                        longestWords.Add(s[i]);
+                    }
+                }
+            }
+
+            // Return a string containing all the longest words from the array.
+            return longestWords;
+        }
+    }
+}
---
+++ H1-MikkelRæv/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace H1_MikkelRæv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tool to find the longest word in a string array
+            LongestWordFinder lwf = new LongestWordFinder();
+
+            // Take user input
+            Console.WriteLine("Indtast et eller flere ord: ");
+            string input = Console.ReadLine();
+
+            // Find the longest word in the input
+            List<string> longestWords = lwf.FindLongestWord(input.Split(' '));
+
+            // Write the result
+            Console.WriteLine("\nDe(t) længste ord i dit input var:\n");
+
+            for (int i = 0; i < longestWords.Count; i++)
+            {
+                Console.WriteLine(longestWords[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
